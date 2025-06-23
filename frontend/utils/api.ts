0b46b5e687f78e38cd0761a9cb904032568159cf
WILDCATZWEB3 <dydxcryptocoin@gmail.com>
2025-06-23 04:39:28 +0100

    Create api.ts

---
+++ frontend/utils/api.ts
@@ -0,0 +1,12 @@
+import axios from 'axios';
+
+const API_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3000/api';
+
+export const api = axios.create({
+  baseURL: API_URL,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
+
+// Interceptors for auth could be added here
